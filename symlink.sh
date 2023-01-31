30b7f03fd57ae64c05e1242d5ba9fab66555f992
Kiyoon Kim <yoonkr33@gmail.com>
2023-01-31 02:34:03 +0000

    .

--- symlink.sh
+++ symlink.sh
@@ -2,14 +2,30 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p ~/.config
-ln -srb "$CURRENT_DIR"/nvim ~/.config
-ln -srb "$CURRENT_DIR"/nvim/.vimrc ~
-ln -srb "$CURRENT_DIR"/tmux/.tmux.conf ~
-ln -srb "$CURRENT_DIR"/oh-my-zsh/.zshrc ~
-ln -srb "$CURRENT_DIR"/oh-my-zsh/starship.toml ~/.config
-ln -srb "$CURRENT_DIR"/wezterm ~/.config
-mkdir -p ~/.cargo
-ln -srb "$CURRENT_DIR"/cargo/config.toml ~/.cargo
-ln -srb "$CURRENT_DIR"/conda/.condarc ~
-# ln -srb git/.gitconfig ~
+# Same as ln -sb (backup if file exists)
+# but mac compatible
+# note that the second argument must be a directory.
+ln_sb() {
+	file_relpath="$1"
+	dest_dir="$2"
+
+	basename="$(basename "$file_relpath")"
+	dest_file="$dest_dir/$basename"
+
+	mkdir -p "$dest_dir"
+	if [[ -f "$dest_file" ]] || [[ -d "$dest_file" ]]
+	then
+		mv "$dest_file" "$dest_file"~
+	fi
+
+	ln -s "$CURRENT_DIR/$file_relpath" "$dest_dir"
+}
+
+ln_sb nvim ~/.config
+ln_sb nvim/.vimrc ~
+ln_sb tmux/.tmux.conf ~
+ln_sb oh-my-zsh/.zshrc ~
+ln_sb oh-my-zsh/starship.toml ~/.config
+ln_sb wezterm ~/.config
+ln_sb cargo/config.toml ~/.cargo
+ln_sb conda/.condarc ~

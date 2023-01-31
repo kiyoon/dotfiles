29ea8b2954b2479209b4559d5d8df9bf2dca4d6c
Kiyoon Kim <yoonkr33@gmail.com>
2023-01-31 03:58:08 +0000

    open nvim tree

--- nvim/lua/user/nvim_tree.lua
+++ nvim/lua/user/nvim_tree.lua
@@ -4,6 +4,27 @@ if not config_status_ok then
   return
 end
 
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_augroup("nvim_tree_open", {})
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = open_nvim_tree,
+  group = "nvim_tree_open",
+})
+
 nvim_tree.setup {
   update_focused_file = {
     enable = true,

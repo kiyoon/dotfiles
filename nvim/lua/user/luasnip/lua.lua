3993bf88108e3cb9a19a538f755a654197e64de8
Kiyoon Kim <yoonkr33@gmail.com>
2023-02-03 16:25:58 +0000

    lua snippet

--- nvim/lua/user/luasnip/lua.lua
+++ nvim/lua/user/luasnip/lua.lua
@@ -1,8 +1,59 @@
 local ls = require "luasnip"
 local fmt = require("luasnip.extras.fmt").fmt
+local extras = require "luasnip.extras"
+local l = extras.lambda
 local i = ls.insert_node
 
 local s = ls.snippet
+
+-- require snippets
+ls.add_snippets("lua", {
+  s(
+    "rq",
+    fmt(
+      [[
+      local {module} = require "{1}"
+    ]],
+      {
+        i(1, "module"),
+        module = l(l._1:gsub("-", "_"):gsub("%.", "_"), 1),
+      }
+    )
+  ),
+})
+
+ls.add_snippets("lua", {
+  s(
+    "prq",
+    fmt(
+      [[
+      local status, {module} = pcall(require, "{1}")
+      if not status then
+        {2}
+      end
+    ]],
+      {
+        i(1, "module"),
+        i(2, "return"),
+        module = l(l._1:gsub("-", "_"):gsub("%.", "_"), 1),
+      }
+    )
+  ),
+})
+
+ls.add_snippets("lua", {
+  s(
+    "pinsp",
+    fmt(
+      [[
+      print(vim.inspect({1}))
+    ]],
+      {
+        i(1, "data"),
+      }
+    )
+  ),
+})
 
 -- add which-key.nvim names
 ls.add_snippets("lua", {

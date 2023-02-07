0b14c57f36398e8a3f3aa1101bff1c4d55ab9c1d
Kiyoon Kim <yoonkr33@gmail.com>
2023-02-07 20:17:12 +0000

    .

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -22,7 +22,9 @@ null_ls.setup {
     formatting.google_java_format,
     diagnostics.flake8.with {
       extra_args = {
-        "--extend-ignore=F821,E203,E266,E501,W503,B905", -- ignore undefined name errors because pyright handles them
+        -- B905: ignore undefined name errors because pyright handles them
+        -- F401: ignore unused imports because pyright handles them
+        "--extend-ignore=F821,E203,E266,E501,W503,B905,F401",
         "--max-line-length=88", -- black style
       },
     },

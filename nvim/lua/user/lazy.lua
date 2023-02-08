2f6bc9d98b48de080b2f976ca12447bba4b3d05c
Kiyoon Kim <yoonkr33@gmail.com>
2023-02-08 17:46:55 +0000

    osc52 copy clipboard from ssh

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -955,4 +955,19 @@ return {
     ft = "python",
     event = "VeryLazy",
   },
+  {
+    "ojroques/nvim-osc52",
+    event = "TextYankPost",
+    config = function()
+      -- every time you yank to + register, copy it to system clipboard
+      -- it works within SSH session and the terminal will use the local clipboard.
+      local function copy()
+        if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
+          require("osc52").copy_register "+"
+        end
+      end
+
+      vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
+    end,
+  },
 }

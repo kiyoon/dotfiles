c8bc3e773adcfcd42f9d53b1aa5de59334023dd7
Kiyoon Kim <yoonkr33@gmail.com>
2023-02-11 13:06:58 +0000

    .

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -126,7 +126,11 @@ return {
   },
   {
     "github/copilot.vim",
-    event = "VeryLazy",
+    event = "InsertEnter",
+    init = function()
+      vim.g.copilot_no_tab_map = true
+      vim.cmd [[imap <silent><script><expr> <C-s> copilot#Accept("")]]
+    end,
   },
   -- "Exafunction/codeium.vim",
   {

f825b0f984a422a64afb2fa200bf6291eb511660
Kiyoon Kim <yoonkr33@gmail.com>
2023-02-13 16:28:40 +0000

    .

---
+++ rsync_cli_plugins.sh
@@ -0,0 +1,20 @@
+if [[ $# -ne 1 ]]
+then
+	echo "Usage: $0 <ssh_name>"
+	echo "Synchronise ~/.local/bin, ~/.local/share/nvim, ~/.cargo/bin, ~/.tmux to remote server"
+	echo "ssh_name is the name of the ssh config entry"
+	echo "Useful for servers that don't have good internet access"
+	exit 1
+fi
+
+ssh_name="$1"
+
+rsync -avz ~/.local/bin $ssh_name:~/.local/
+rsync -avz ~/.local/include $ssh_name:~/.local
+rsync -avz ~/.local/lib $ssh_name:~/.local
+rsync -avz ~/.local/man $ssh_name:~/.local
+rsync -avz ~/.local/share/nvim $ssh_name:~/.local/share
+rsync -avz ~/.local/share/terminfo $ssh_name:~/.local/share
+rsync -avz ~/.local/share/man $ssh_name:~/.local/share
+rsync -avz ~/.cargo/bin $ssh_name:~/.cargo/
+rsync -avz ~/.tmux $ssh_name:~/

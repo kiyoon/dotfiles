0cdbc2e6cb87d3e1810230be8b8dab84600c20f8
Kiyoon Kim <yoonkr33@gmail.com>
2023-02-17 22:01:00 +0000

    .

--- nvim/lua/kiyoon/lazy.lua
+++ nvim/lua/kiyoon/lazy.lua
@@ -1228,4 +1228,70 @@ return {
       require("nvim-autopairs").setup {}
     end,
   },
+  {
+    "ziontee113/SelectEase",
+    event = "VeryLazy",
+    config = function()
+      local select_ease = require "SelectEase"
+
+      local lua_query = [[
+          ;; query
+          ((identifier) @cap)
+          ("string_content" @cap)
+          ((true) @cap)
+          ((false) @cap)
+          ]]
+      local python_query = [[
+          ;; query
+          ((identifier) @cap)
+          ((string) @cap)
+          ]]
+
+      local queries = {
+        lua = lua_query,
+        python = python_query,
+      }
+
+      vim.keymap.set({ "n", "s", "i" }, "<C-A-k>", function()
+        select_ease.select_node {
+          queries = queries,
+          direction = "previous",
+          vertical_drill_jump = true,
+          -- visual_mode = true, -- if you want Visual Mode instead of Select Mode
+        }
+      end, {})
+      vim.keymap.set({ "n", "s", "i" }, "<C-A-j>", function()
+        select_ease.select_node {
+          queries = queries,
+          direction = "next",
+          vertical_drill_jump = true,
+          -- visual_mode = true, -- if you want Visual Mode instead of Select Mode
+        }
+      end, {})
+      vim.keymap.set({ "n", "s", "i" }, "<C-A-h>", function()
+        select_ease.select_node {
+          queries = queries,
+          direction = "previous",
+          current_line_only = true,
+          -- visual_mode = true, -- if you want Visual Mode instead of Select Mode
+        }
+      end, {})
+      vim.keymap.set({ "n", "s", "i" }, "<C-A-l>", function()
+        select_ease.select_node {
+          queries = queries,
+          direction = "next",
+          current_line_only = true,
+          -- visual_mode = true, -- if you want Visual Mode instead of Select Mode
+        }
+      end, {})
+
+      -- previous / next node that matches query
+      vim.keymap.set({ "n", "s", "i" }, "<C-A-p>", function()
+        select_ease.select_node { queries = queries, direction = "previous" }
+      end, {})
+      vim.keymap.set({ "n", "s", "i" }, "<C-A-n>", function()
+        select_ease.select_node { queries = queries, direction = "next" }
+      end, {})
+    end,
+  },
 }

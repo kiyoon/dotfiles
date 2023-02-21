0af19b7ca11880282c4df26e85ce18b82b445997
Kiyoon Kim <yoonkr33@gmail.com>
2023-02-21 16:43:58 +0000

    .

--- nvim/lua/kiyoon/lazy.lua
+++ nvim/lua/kiyoon/lazy.lua
@@ -733,7 +733,9 @@ return {
       {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
-        enabled = vim.fn.executable "make" == 1,
+        cond = function()
+          return vim.fn.executable "make" == 1
+        end,
         config = function()
           require("telescope").load_extension "fzf"
         end,

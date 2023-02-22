65f0257d43d0134b5b67f03f8f93701458b7022d
Kiyoon Kim <yoonkr33@gmail.com>
2023-02-22 19:17:39 +0000

    .

--- nvim/lua/kiyoon/bufferline_opts.lua
+++ nvim/lua/kiyoon/bufferline_opts.lua
@@ -1,3 +1,5 @@
+local icons = require "kiyoon.icons"
+
 return {
   options = {
     right_mouse_command = "", -- can be a string | function, see "Mouse actions"
@@ -5,7 +7,7 @@ return {
     always_show_bufferline = false,
     diagnostics_indicator = function(_, _, diag)
       local ret = (diag.error and icons.diagnostics.Error .. diag.error .. " " or "")
-      .. (diag.warning and icons.diagnostics.Warn .. diag.warning or "")
+        .. (diag.warning and icons.diagnostics.Warn .. diag.warning or "")
       return vim.trim(ret)
     end,
     offsets = {

ee921def7c30ea203898dcf54a7f15c6851e9ffe
Kiyoon Kim <yoonkr33@gmail.com>
2023-04-04 12:14:24 +0100

    fix(wezterm): github link more constrained

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 
-return {
+local config = {
 	color_scheme = "Dracula (Official)",
 
 	font = wezterm.font_with_fallback({
@@ -8,53 +8,6 @@ return {
 		"Fira Code Nerd Font",
 	}),
 	font_size = 14.3,
-
-	hyperlink_rules = {
-		-- Linkify things that look like URLs and the host has a TLD name.
-		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-		{
-			regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
-			format = "$0",
-		},
-
-		-- linkify email addresses
-		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-		{
-			regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
-			format = "mailto:$0",
-		},
-
-		-- file:// URI
-		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-		{
-			regex = [[\bfile://\S*\b]],
-			format = "$0",
-		},
-
-		-- Linkify things that look like URLs with numeric addresses as hosts.
-		-- E.g. http://127.0.0.1:8000 for a local development server,
-		-- or http://192.168.1.1 for the web interface of many routers.
-		{
-			regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-			format = "$0",
-		},
-
-		-- Make task numbers clickable
-		-- The first matched regex group is captured in $1.
-		-- {
-		-- 	regex = [[\b[tT](\d+)\b]],
-		-- 	format = "https://example.com/tasks/?t=$1",
-		-- },
-
-		-- Make username/project paths clickable. This implies paths like the following are for GitHub.
-		-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
-		-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
-		-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
-		{
-			regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-			format = "https://www.github.com/$1/$3",
-		},
-	},
 
 	-- undercurl becomes ugly if underline_position < -4
 	underline_position = -4,
@@ -66,3 +19,15 @@ return {
 		},
 	},
 }
+
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+table.insert(config.hyperlink_rules, {
+	regex = [[["'\s]([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["'\s]] .. "]",
+	format = "https://www.github.com/$1/$3",
+})
+
+return config

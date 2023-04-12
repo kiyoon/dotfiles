84e5399192c06730cd13b9b879cc9d1732f87487
Kiyoon Kim <yoonkr33@gmail.com>
2023-04-12 13:49:56 +0100

    .

---
+++ wezterm/install-ubuntu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+TEMP_DIR=$(mktemp -d)
+curl -s https://api.github.com/repos/wez/wezterm/releases/latest \
+  | grep "browser_download_url.*.Ubuntu22.04.deb" \
+  | cut -d : -f 2,3 \
+  | tr -d \" \
+  | wget -qi - -O "$TEMP_DIR/wezterm.deb"
+
+sudo dpkg -i "$TEMP_DIR/wezterm.deb"

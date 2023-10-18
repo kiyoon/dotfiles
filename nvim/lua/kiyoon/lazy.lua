aa4761191bd3562272faf2403ee8bcb5977edd81
Kiyoon Kim <yoonkr33@gmail.com>
2023-10-18 04:32:44 +0000

    fix: indent-blankline fix to v2

--- nvim/lua/kiyoon/lazy.lua
+++ nvim/lua/kiyoon/lazy.lua
@@ -491,12 +491,39 @@ return {
   },
   {
     "lukas-reineke/indent-blankline.nvim",
+    tag = "v2.20.8",
+    -- main = "ibl",
+    -- opts = {},
     event = "BufReadPost",
     config = function()
       vim.opt.list = true
       --vim.opt.listchars:append "space:⋅"
       --vim.opt.listchars:append "eol:↴"
 
+      -- local highlight = {
+      --   "RainbowDelimiterRed",
+      --   "RainbowDelimiterOrange",
+      --   "RainbowDelimiterYellow",
+      --   "RainbowDelimiterGreen",
+      --   "RainbowDelimiterBlue",
+      --   "RainbowDelimiterCyan",
+      --   "RainbowDelimiterViolet",
+      -- }
+      -- local hooks = require "ibl.hooks"
+      -- -- create the highlight groups in the highlight setup hook, so they are reset
+      -- -- every time the colorscheme changes
+      -- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      --     vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+      --     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      --     vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+      --     vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      --     vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+      --     vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      --     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      -- end)
+      --
+      -- require("ibl").setup { scope = { highlight = highlight } }
+      -- hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
       require("indent_blankline").setup {
         space_char_blankline = " ",
         show_current_context = true,

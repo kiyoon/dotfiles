0064b4d424ee74d9dc8bc76f7e9f340534944e87
Kiyoon Kim <yoonkr33@gmail.com>
2023-10-26 15:11:09 +0900

    fix: isort removing everything when it errors

--- nvim/lua/kiyoon/lsp/null-ls.lua
+++ nvim/lua/kiyoon/lsp/null-ls.lua
@@ -18,16 +18,72 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+-- make custom isort source, because it will remove everything when there is an error
+local h = require "null-ls.helpers"
+local methods = require "null-ls.methods"
+local u = require "null-ls.utils"
+
+local FORMATTING = methods.internal.FORMATTING
+
+local isort = {
+  name = "isort",
+  meta = {
+    url = "https://github.com/PyCQA/isort",
+    description = "Python utility / library to sort imports alphabetically and automatically separate them into sections and by type.",
+  },
+  method = FORMATTING,
+  filetypes = { "python" },
+  generator = null_ls.generator {
+    command = "isort",
+    args = {
+      "--profile=black",
+      "--stdout",
+      "--filename",
+      "$FILENAME",
+      "-",
+    },
+    to_stdin = true,
+    cwd = h.cache.by_bufnr(function(params)
+      return u.root_pattern(
+        -- isort will detect files in the CWD as first-party
+        -- https://pycqa.github.io/isort/docs/configuration/config_files.html
+        ".isort.cfg",
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "tox.ini",
+        ".editorconfig"
+      )(params.bufname)
+    end),
+    factory = h.formatter_factory,
+    check_exit_code = function(code, stderr)
+      local success = code <= 1
+
+      if not success then
+        -- can be noisy for things that run often (e.g. diagnostics), but can
+        -- be useful for things that run on demand (e.g. formatting)
+        print(stderr)
+      end
+
+      return success
+    end,
+    on_output = function(params, done)
+      local output = params.output
+      if not output then
+        return done()
+      end
+
+      return done { { text = output } }
+    end,
+  },
+}
+
 local sources = {
   formatting.prettier.with {
     extra_filetypes = { "toml" },
     extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
   },
-  formatting.isort.with {
-    extra_args = {
-      "--profile=black",
-    },
-  },
+  isort,
   formatting.black,
   formatting.stylua,
   formatting.google_java_format,
@@ -64,7 +120,8 @@ end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup {
-  debug = false,
+  debug = true,
+  ignore_stderr = false,
   sources = sources,
   -- format on save
   on_attach = function(client, bufnr)

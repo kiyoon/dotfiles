fbd2b13ff582ae591b60c61816cf390bef760b94
Kiyoon Kim <yoonkr33@gmail.com>
2023-11-27 20:55:42 +0900

    feat: no flash.nvim search (use s instead)

--- nvim/lua/kiyoon/lazy.lua
+++ nvim/lua/kiyoon/lazy.lua
@@ -757,7 +757,13 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
+    opts = {
+      modes = {
+        search = {
+          enabled = false,
+        },
+      },
+    },
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },

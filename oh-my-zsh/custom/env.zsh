d13a340d6ba100e0eb87676f10b54bdac0b41bb9
Kiyoon Kim <yoonkr33@gmail.com>
2023-12-21 16:42:23 +0900

    feat: bat theme to dracula

--- oh-my-zsh/custom/env.zsh
+++ oh-my-zsh/custom/env.zsh
@@ -3,6 +3,7 @@ export LD_LIBRARY_PATH="$HOME/.local/lib:$LD_LIBRARY_PATH"
 export MANPATH="$HOME/.local/share/man:$MANPATH"
 
 export TERMINFO="$HOME/.local/share/terminfo" # tmux needs this
+export BAT_THEME="Dracula"
 
 if (($+commands[nvim])); then
 	export SUDO_EDITOR="$commands[nvim]"

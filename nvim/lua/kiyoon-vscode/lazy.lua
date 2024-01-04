ffe432e9923f49457d7dc330d1ccd403a5db32c7
Kiyoon Kim <yoonkr33@gmail.com>
2024-01-04 23:36:21 +0900

    feat: flash.nvim for vscode

--- nvim/lua/kiyoon-vscode/lazy.lua
+++ nvim/lua/kiyoon-vscode/lazy.lua
@@ -10,10 +10,6 @@ return {
       { "<M-CR>", "<Esc>:ImportSymbol<CR>a", mode = "i" },
     },
   },
-  {
-    "Vimjas/vim-python-pep8-indent",
-    ft = "python",
-  },
   --- NOTE: Coding
   {
     -- <space>siwie to substitute word from entire buffer
@@ -189,67 +185,24 @@ return {
     },
   },
   {
-    "mfussenegger/nvim-treehopper",
-    dependencies = {
-      {
-        "phaazon/hop.nvim",
-        config = function()
-          require("hop").setup()
-        end,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        search = {
+          enabled = false,
+        },
       },
     },
+    -- stylua: ignore
     keys = {
-      {
-        "m",
-        "<Cmd>lua require('tsht').nodes()<CR>",
-        mode = "o",
-        desc = "TreeSitter [M]otion",
-      },
-      {
-        "m",
-        ":lua require('tsht').nodes()<CR>",
-        mode = "x",
-        noremap = true,
-        desc = "TreeSitter [M]otion",
-      },
-      { "m", "<Cmd>lua require('tsht').move({ side = 'start' })<CR>", desc = "TreeSitter [M]otion" },
-      { "M", "m", noremap = true, desc = "[M]ark" },
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
-  },
-  {
-    "ggandor/leap.nvim",
-    keys = {
-      { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-      { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
-    },
-    dependencies = {
-      "tpope/vim-repeat",
-    },
-    config = function(_, opts)
-      local leap = require "leap"
-      for k, v in pairs(opts) do
-        leap.opts[k] = v
-      end
-      leap.add_default_mappings()
-      vim.keymap.del({ "x", "o" }, "x")
-      vim.keymap.del({ "x", "o" }, "X")
-
-      -- x to delete without yanking
-      vim.keymap.set({ "n", "x" }, "x", [["_x]], { noremap = true })
-    end,
-  },
-  {
-    "ggandor/flit.nvim",
-    keys = function()
-      ---@type LazyKeys[]
-      local ret = {}
-      for _, key in ipairs { "f", "F", "t", "T" } do
-        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
-      end
-      return ret
-    end,
-    opts = { labeled_modes = "nv" },
   },
   {
     "Wansmer/sibling-swap.nvim",
--- wezterm/terminfo.sh
+++ wezterm/terminfo.sh
@@ -1,4 +1,4 @@
 tempfile=$(mktemp) &&
 	curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/master/termwiz/data/wezterm.terminfo &&
-	tic -x -o ~/.terminfo $tempfile &&
+	tic -x -o ~/.local/share/terminfo $tempfile &&
 	rm $tempfile

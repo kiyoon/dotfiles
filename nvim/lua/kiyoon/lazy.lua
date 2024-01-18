fadc657c005a10a28fd5167e9e4b4d5010458eca
Kiyoon Kim <yoonkr33@gmail.com>
2024-01-18 17:23:49 +0900

    fix: disable buggy feature

--- nvim/lua/kiyoon/lazy.lua
+++ nvim/lua/kiyoon/lazy.lua
@@ -1640,15 +1640,15 @@ return {
     "3rd/image.nvim",
     opts = {
       backend = "kitty",
-      -- integrations = {
-      --   markdown = {
-      --     enabled = true,
-      --     clear_in_insert_mode = false,
-      --     download_remote_images = true,
-      --     only_render_image_at_cursor = false,
-      --     filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-      --   },
-      -- },
+      integrations = {
+        markdown = {
+          enabled = false,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+        },
+      },
       max_width = 100,
       max_height = 12,
       max_height_window_percentage = math.huge,

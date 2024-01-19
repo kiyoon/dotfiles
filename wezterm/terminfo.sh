2e4bf38084965f50fe9506922cbe324078be8910
Kiyoon Kim <yoonkr33@gmail.com>
2024-01-19 09:56:46 +0900

    feat: wezterm terminfo to ssh

--- wezterm/terminfo.sh
+++ wezterm/terminfo.sh
@@ -1,5 +1,17 @@
-tempfile=$(mktemp) &&
-	curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/master/termwiz/data/wezterm.terminfo &&
-	tic -x -o ~/.local/share/terminfo $tempfile &&
-	tic -x -o ~/.terminfo $tempfile &&
-	rm $tempfile
+if [[ $# -eq 0 ]] ; then
+	tempfile=$(mktemp) &&
+		curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/master/termwiz/data/wezterm.terminfo &&
+		tic -x -o ~/.local/share/terminfo $tempfile &&
+		tic -x -o ~/.terminfo $tempfile &&
+		rm $tempfile
+else
+	# the first argument is the ssh host
+	ssh "$1" 'tempfile=$(mktemp) &&
+		curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/master/termwiz/data/wezterm.terminfo &&
+		tic -x -o ~/.local/share/terminfo $tempfile &&
+		tic -x -o ~/.terminfo $tempfile &&
+		rm $tempfile'
+fi
+
+
+

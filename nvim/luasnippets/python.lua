1c8bdb9a80e5c7048eea211d03c2121e551ef8b2
Kiyoon Kim <yoonkr33@gmail.com>
2024-01-21 11:03:27 +0000

    feat: logmain with logging from module

--- nvim/luasnippets/python.lua
+++ nvim/luasnippets/python.lua
@@ -1,8 +1,54 @@
 local ls = require "luasnip"
 local i = ls.insert_node
+local func_node = ls.function_node
 
 local s = ls.snippet
 local t = ls.text_node
+
+local function find_python_first_party_modules()
+  -- find src/module_name in git root
+
+  local git_root = vim.fn.systemlist "git rev-parse --show-toplevel"
+  if #git_root == 0 then
+    return nil
+  end
+  git_root = git_root[1]
+
+  local src_dir = git_root .. "/src"
+  if vim.fn.isdirectory(src_dir) == 0 then
+    return nil
+  end
+
+  local modules = {}
+  local function find_modules(dir)
+    local files = vim.fn.readdir(dir)
+    for _, file in ipairs(files) do
+      local path = dir .. "/" .. file
+      local stat = vim.loop.fs_stat(path)
+      if stat.type == "directory" then
+        -- no egg-info
+        if file:match "%.egg%-info$" == nil then
+          modules[#modules + 1] = file
+        end
+      end
+    end
+  end
+  find_modules(src_dir)
+
+  if #modules == 0 then
+    return nil
+  end
+
+  return modules
+end
+
+local function find_first_party_module()
+  local modules = find_python_first_party_modules()
+  if modules == nil then
+    return nil
+  end
+  return modules[1]
+end
 
 return {
   s("li", {
@@ -62,6 +108,12 @@ return {
       "",
       "import logging",
       "",
+      "from ",
+    },
+    func_node(find_first_party_module, {}),
+    t {
+      ".utils import setup_logging",
+      "",
       "logger = logging.getLogger(__name__)",
       "",
       "",
@@ -75,6 +127,7 @@ return {
       "",
       [[if __name__ == "__main__":]],
       "\ttry:",
+      "\t\tsetup_logging()",
       "\t\tmain()",
       "\texcept Exception:",
       "\t\t" .. [[logger.exception("Exception occurred")]],

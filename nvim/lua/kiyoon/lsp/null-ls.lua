b1b810b8bf1ec5bfcb53f8386587892fbfc4dd06
Kiyoon Kim <yoonkr33@gmail.com>
2024-01-25 10:54:08 +0900

    fix: duplicated diagnostics in ruff and pyright

--- nvim/lua/kiyoon/lsp/null-ls.lua
+++ nvim/lua/kiyoon/lsp/null-ls.lua
@@ -16,12 +16,12 @@ end
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 -- local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-local diagnostics = null_ls.builtins.diagnostics
+-- local diagnostics = null_ls.builtins.diagnostics
 
 -- make custom isort source, because it will remove everything when there is an error
 local h = require "null-ls.helpers"
 local methods = require "null-ls.methods"
-local u = require "null-ls.utils"
+-- local u = require "null-ls.utils"
 
 -- local FORMATTING = methods.internal.FORMATTING
 --
@@ -78,6 +78,137 @@ local u = require "null-ls.utils"
 --   },
 -- }
 
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+local custom_end_col = {
+  end_col = function(entries, line)
+    if not line then
+      return
+    end
+
+    local start_col = entries["col"]
+    local message = entries["message"]
+    local code = entries["code"]
+    local default_position = start_col + 1
+
+    local pattern = nil
+    local trimmed_line = line:sub(start_col, -1)
+
+    if code == "F841" or code == "F823" then
+      pattern = [[Local variable %`(.*)%`]]
+    elseif code == "F821" or code == "F822" then
+      pattern = [[Undefined name %`(.*)%`]]
+    elseif code == "F401" then
+      pattern = [[%`(.*)%` imported but unused]]
+    elseif code == "F841" then
+      pattern = [[Local variable %`(.*)%` is assigned to but never used]]
+    end
+    if not pattern then
+      return default_position
+    end
+
+    local results = message:match(pattern)
+    local _, end_col = trimmed_line:find(results, 1, true)
+
+    if not end_col then
+      return default_position
+    end
+
+    end_col = end_col + start_col
+    if end_col > tonumber(start_col) then
+      return end_col
+    end
+
+    return default_position
+  end,
+}
+
+-- Copied from null-ls builtins, but filters out some diagnostics.
+-- If you simply add --extend-ignore=CODE to the ruff args, it will remove those diagnostics.
+-- HOWEVER, it will create warning when you use # noqa: CODE, which is not ideal.
+-- This is a workaround for that.
+local function ruff_on_output_filtered(pattern, groups)
+  local diag_func = h.diagnostics.from_pattern(
+    [[(%d+):(%d+): ((%u)%w+) (.*)]],
+    { "row", "col", "code", "severity", "message" },
+    {
+      adapters = {
+        custom_end_col,
+      },
+      severities = {
+        E = h.diagnostics.severities["error"], -- pycodestyle errors
+        W = h.diagnostics.severities["warning"], -- pycodestyle warnings
+        F = h.diagnostics.severities["information"], -- pyflakes
+        A = h.diagnostics.severities["information"], -- flake8-builtins
+        B = h.diagnostics.severities["warning"], -- flake8-bugbear
+        C = h.diagnostics.severities["warning"], -- flake8-comprehensions
+        T = h.diagnostics.severities["information"], -- flake8-print
+        U = h.diagnostics.severities["information"], -- pyupgrade
+        D = h.diagnostics.severities["information"], -- pydocstyle
+        M = h.diagnostics.severities["information"], -- Meta
+      },
+    }
+  )
+
+  -- F821: ignore undefined name errors because pyright handles them
+  -- B905: ignore undefined name errors because pyright handles them
+  -- F401: ignore unused imports because pyright handles them
+  -- F841: ignore unused variables because pyright handles them
+  local filter_out_codes = {
+    F821 = true,
+    B905 = true,
+    F401 = true,
+    F841 = true,
+  }
+
+  local one_diag = diag_func(pattern, groups)
+  if one_diag ~= nil and filter_out_codes[one_diag["code"]] then
+    return nil
+  end
+
+  return one_diag
+end
+
+local ruff_diagnostics_filtered = {
+  name = "ruff",
+  meta = {
+    url = "https://github.com/charliermarsh/ruff/",
+    description = "An extremely fast Python linter, written in Rust.",
+  },
+  method = DIAGNOSTICS,
+  filetypes = { "python" },
+  generator = null_ls.generator {
+    command = "ruff",
+    args = {
+      "-n",
+      "-e",
+      "--stdin-filename",
+      "$FILENAME",
+      "-",
+    },
+    format = "line",
+    -- cwd = h.cache.by_bufnr(function(params)
+    --   return u.root_pattern(
+    --     -- isort will detect files in the CWD as first-party
+    --     -- https://pycqa.github.io/isort/docs/configuration/config_files.html
+    --     ".isort.cfg",
+    --     "pyproject.toml",
+    --     "setup.py",
+    --     "setup.cfg",
+    --     "tox.ini",
+    --     ".editorconfig"
+    --   )(params.bufname)
+    -- end),
+    factory = h.generator_factory,
+    check_exit_code = function(code)
+      return code == 0
+    end,
+    to_stdin = true,
+    ignore_stderr = true,
+    on_output = ruff_on_output_filtered,
+  },
+  factory = h.generator_factory,
+}
+
 local python_tools_code_action = {
   name = "python_tools_code_action",
   meta = {
@@ -110,15 +241,16 @@ local sources = {
   -- formatting.black,
   -- formatting.stylua,
   -- formatting.google_java_format,
-  diagnostics.ruff.with {
-    extra_args = {
-      -- F821: ignore undefined name errors because pyright handles them
-      -- B905: ignore undefined name errors because pyright handles them
-      -- F401: ignore unused imports because pyright handles them
-      -- F841: ignore unused variables because pyright handles them
-      -- "--extend-ignore=F821,B905,F401,F841",
-    },
-  },
+  -- diagnostics.ruff.with {
+  --   extra_args = {
+  --     -- F821: ignore undefined name errors because pyright handles them
+  --     -- B905: ignore undefined name errors because pyright handles them
+  --     -- F401: ignore unused imports because pyright handles them
+  --     -- F841: ignore unused variables because pyright handles them
+  --     "--extend-ignore=F821,B905,F401,F841",
+  --   },
+  -- },
+  ruff_diagnostics_filtered,
   -- diagnostics.flake8.with {
   --   extra_args = {
   --     -- B905: ignore undefined name errors because pyright handles them

df0ade3ee6afa472f046946709f034e6eddb3669
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-01-31 15:13:35 +0900

    .

--- oh-my-zsh/custom/_env.zsh
+++ oh-my-zsh/custom/_env.zsh
@@ -116,9 +116,17 @@ if (($+commands[eza])); then
 	if (($+commands[bat])); then
 		# Preview on cd with eza
 		# zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -w $(( $(tput cols)/2 - 3 )) --color=always --git-ignore $realpath'
-		zstyle ':fzf-tab:complete:*' fzf-preview 'if [[ -d $realpath ]]; then \
-				eza -w $(( $(tput cols)/2 - 3 )) --color=always --git-ignore $realpath; \
-			else bat --color=always --style=numbers --line-range=:1000 $realpath; fi'
+		zstyle ':fzf-tab:complete:*' fzf-preview '\
+			if [[ -d $realpath ]]; then \
+				preview_width=$(( $(tput cols)/2 - 4 )); \
+				if [[ $preview_width -gt 20 ]]; then \
+					eza -w $preview_width --icons=always --color=always --git-ignore $realpath; \
+				else \
+					eza -w $preview_width --color=always --git-ignore $realpath; \
+				fi; \
+			else \
+				bat --color=always --style=numbers --line-range=:1000 $realpath; \
+			fi'
 	fi
 fi
 

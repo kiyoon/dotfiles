89dfce8e79efc5ab2251b63d49935c66b1cc2223
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-02-02 14:33:28 +0900

    .

--- nvim/lua/kiyoon/lazy.lua
+++ nvim/lua/kiyoon/lazy.lua
@@ -1626,6 +1626,23 @@ return {
     "mechatroner/rainbow_csv",
     ft = "csv",
   },
+  {
+    "fei6409/log-highlight.nvim",
+    config = function()
+      require("log-highlight").setup {
+        -- The file extensions.
+        extension = "log",
+
+        -- The file path glob patterns, e.g. `.*%.lg`, `/var/log/.*`.
+        -- Note: `%.` is to match a literal dot (`.`) in a pattern in Lua, but most
+        -- of the time `.` and `%.` here make no observable difference.
+        pattern = {
+          "/var/log/.*",
+          "messages%..*",
+        },
+      }
+    end,
+  },
   -- NOTE: for kiyoon/treemux
   -- You need to have them installed but not using them with `cond = false`
   {

3757351a5835e8d2c9d4d2fa3da3bf80b23042e3
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-02-21 17:59:50 +0900

    .

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -13,14 +13,24 @@ fi
 
 if (($+commands[nvim])); then
 	vi() {
-		if [[ "$#" -ge 2 ]] && [[ "$1" == "vi" ]]; then
-			# Likely a typo because vi is typed twice.
-			# ignore the first argument, and pass the rest to nvim
-			# ${@:2} = slice from second to the last
-			nvim "${@:2}"
-		else
-			nvim "$@"
+		if [[ "$#" -ge 2 ]]; then
+			if [[ "$1" == "vi" ]]; then
+				# Likely a typo because vi is typed twice.
+				# ignore the first argument, and pass the rest to nvim
+				# ${@:2} = slice from second to the last
+				nvim "${@:2}"
+				return
+			elif [[ "$1" == "ls" ]]; then
+				ls "${@:2}"
+				return
+			fi
+		elif [[ "$#" -eq 1 ]]; then
+			if [[ "$1" == "ls" ]]; then
+				ls
+				return
+			fi
 		fi
+		nvim "$@"
 	}
 	alias v='nvim'
 	alias vim='nvim'

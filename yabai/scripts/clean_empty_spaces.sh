aa572fb494e7660a34a5e9e838bb30ad6011bfd7
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-03-13 14:01:02 +0900

    .

--- yabai/scripts/clean_empty_spaces.sh
+++ yabai/scripts/clean_empty_spaces.sh
@@ -5,5 +5,6 @@
 yabai -m query --spaces --display | \
      jq -re 'map(select(."is-native-fullscreen" == false)) | length > 1' \
      && yabai -m query --spaces | \
-          jq -re 'map(select(."windows" == [] and ."has-focus" == false).index) | reverse | .[] ' | \
+          jq -re 'map(select(."windows" == []).index) | reverse | .[] ' | \
           xargs -I % sh -c 'yabai -m space % --destroy'
+          # jq -re 'map(select(."windows" == [] and ."has-focus" == false).index) | reverse | .[] ' | \

7a2e6aee064c098a433d866a4b41e9d243987002
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-03-23 01:30:57 +0900

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -199,5 +199,5 @@ vim.keymap.set({ "n", "v", "o" }, "<F6>", function()
 end, { desc = "tmux next window" })
 
 -- Align CSV columns. Much faster than rainbow_csv
--- https://stackoverflow.com/questions/3249275/multiple-commands-on-same-line
+-- https://stackoverflow.com/questions/51471554/align-columns-in-comma-separated-file
 vim.api.nvim_create_user_command("CsvAlign", ":set nowrap | %!sed 's/,/:,/g' | column -t -s: | sed 's/ ,/,/g'<cr>", {})

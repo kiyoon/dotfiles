4a35239cc3951375a6fde39a9381724911fec948
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-03-25 12:39:22 +0900

    .

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -129,8 +129,40 @@ idamnii() {
 }
 
 if [[ -n "$TMUX" ]]; then
+	# get pwd of another pane (left, right, top, bottom)
 	alias tpl='tmux display-message -p -F "#{pane_current_path}" -t left'
 	alias tpr='tmux display-message -p -F "#{pane_current_path}" -t right'
 	alias tpb='tmux display-message -p -F "#{pane_current_path}" -t bottom'
 	alias tpt='tmux display-message -p -F "#{pane_current_path}" -t top'
+
+	# send pwd to another pane (left, right, top, bottom)
+	alias stpl='tmux send-keys -t left "$(pwd)"'
+	alias stpr='tmux send-keys -t right "$(pwd)"'
+	alias stpb='tmux send-keys -t bottom "$(pwd)"'
+	alias stpt='tmux send-keys -t top "$(pwd)"'
+
+	# send pwd to another pane and focus (left, right, top, bottom)
+	alias stplf='tmux send-keys -t left "$(pwd)"; tmux select-pane -t left'
+	alias stprf='tmux send-keys -t right "$(pwd)"; tmux select-pane -t right'
+	alias stpbf='tmux send-keys -t bottom "$(pwd)"; tmux select-pane -t bottom'
+	alias stptf='tmux send-keys -t top "$(pwd)"; tmux select-pane -t top'
+
+	# send pwd to another window (1, 2, 3, ...)
+	function stw() {
+		if [[ "$#" -eq 0 ]]; then
+			echo "Usage: stw <window_number>"
+			return
+		fi
+		tmux send-keys -t $1. "$(pwd)"
+	}
+	
+	# send pwd to another window and focus (1, 2, 3, ...)
+	function stwf() {
+		if [[ "$#" -eq 0 ]]; then
+			echo "Usage: stw <window_number>"
+			return
+		fi
+		tmux send-keys -t $1. "$(pwd)"
+		tmux select-window -t $1.
+	}
 fi

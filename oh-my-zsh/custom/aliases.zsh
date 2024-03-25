64cfcccc398f41c6be30e21314afa605b713cb77
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-03-25 13:04:33 +0900

    .

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -135,11 +135,39 @@ if [[ -n "$TMUX" ]]; then
 	alias tpb='tmux display-message -p -F "#{pane_current_path}" -t bottom'
 	alias tpt='tmux display-message -p -F "#{pane_current_path}" -t top'
 
-	# send pwd to another pane (left, right, top, bottom)
-	alias stpl='tmux send-keys -t left "$(pwd)"'
-	alias stpr='tmux send-keys -t right "$(pwd)"'
-	alias stpb='tmux send-keys -t bottom "$(pwd)"'
-	alias stpt='tmux send-keys -t top "$(pwd)"'
+	# send pwd or absolute file path to another pane
+	function stp() {
+		if [[ "$#" -eq 0 ]]; then
+			echo "Usage: stp <left|right|top|bottom> [file_path (optional)]"
+			return 1
+	 	elif [[ "$#" -eq 1 ]]; then
+			tmux send-keys -t $1 "$(pwd)"
+		else
+			tmux send-keys -t $1 "$(realpath "$2")"
+		fi
+	}
+	alias stpl='stp left'
+	alias stpr='stp right'
+	alias stpb='stp bottom'
+	alias stpt='stp top'
+
+	function stpf() {
+		if [[ "$#" -eq 0 ]]; then
+			echo "Usage: stpf <left|right|top|bottom> [file_path (optional)]"
+			return 1
+		fi
+		stp "$@"
+		tmux select-pane -t $1
+	}
+	alias stpfl='stpf left'
+	alias stpfr='stpf right'
+	alias stpfb='stpf bottom'
+	alias stpft='stpf top'
+
+	# alias stpl='tmux send-keys -t left "$(pwd)"'
+	# alias stpr='tmux send-keys -t right "$(pwd)"'
+	# alias stpb='tmux send-keys -t bottom "$(pwd)"'
+	# alias stpt='tmux send-keys -t top "$(pwd)"'
 
 	# send pwd to another pane and focus (left, right, top, bottom)
 	alias stplf='tmux send-keys -t left "$(pwd)"; tmux select-pane -t left'
@@ -150,19 +178,23 @@ if [[ -n "$TMUX" ]]; then
 	# send pwd to another window (1, 2, 3, ...)
 	function stw() {
 		if [[ "$#" -eq 0 ]]; then
-			echo "Usage: stw <window_number>"
-			return
+			echo "Usage: stw <window_number> [file_path (optional)]"
+			return 1
+		elif [[ "$#" -eq 1 ]]; then
+			tmux send-keys -t $1. "$(pwd)"
+		else
+			tmux send-keys -t $1. "$(realpath "$2")"
 		fi
-		tmux send-keys -t $1. "$(pwd)"
+
 	}
 	
 	# send pwd to another window and focus (1, 2, 3, ...)
 	function stwf() {
 		if [[ "$#" -eq 0 ]]; then
-			echo "Usage: stw <window_number>"
-			return
+			echo "Usage: stwf <window_number> [file_path (optional)]"
+			return 1
 		fi
-		tmux send-keys -t $1. "$(pwd)"
+		stw "$@"
 		tmux select-window -t $1.
 	}
 fi

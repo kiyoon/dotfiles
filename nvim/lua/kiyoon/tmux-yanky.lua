8e9f85f420bde34a01975ad420ec611ff47e5a6b
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-10-02 18:41:06 +0900

    .

--- nvim/lua/kiyoon/tmux-yanky.lua
+++ nvim/lua/kiyoon/tmux-yanky.lua
@@ -16,6 +16,7 @@ require("tmux").setup {
 -- since we want to use sync_registers with yanky.nvim, we need to
 -- configure keybindings manually.
 local yanky = require "yanky"
+local yanky_wrappers = require "yanky.wrappers"
 yanky.setup {
   ring = {
     ignore_registers = { "_", "+", "*" },
@@ -31,6 +32,7 @@ vim.keymap.set("n", "p", function()
     require("tmux.copy").sync_registers()
   end
   yanky.put("p", false)
+  -- yanky.put("p", false)
 end)
 vim.keymap.set("x", "p", function()
   if vim.env.TMUX then
@@ -49,6 +51,33 @@ vim.keymap.set("x", "P", function()
     require("tmux.copy").sync_registers()
   end
   yanky.put("P", true)
+end)
+
+-- blockwise paste
+vim.keymap.set("n", "<leader>p", function()
+  if vim.env.TMUX then
+    require("tmux.copy").sync_registers()
+  end
+  yanky.put("p", false, yanky_wrappers.blockwise())
+  -- yanky.put("p", false)
+end)
+vim.keymap.set("x", "<leader>p", function()
+  if vim.env.TMUX then
+    require("tmux.copy").sync_registers()
+  end
+  yanky.put("p", true, yanky_wrappers.blockwise())
+end)
+vim.keymap.set("n", "<leader>P", function()
+  if vim.env.TMUX then
+    require("tmux.copy").sync_registers()
+  end
+  yanky.put("P", false, yanky_wrappers.blockwise())
+end)
+vim.keymap.set("x", "<leader>P", function()
+  if vim.env.TMUX then
+    require("tmux.copy").sync_registers()
+  end
+  yanky.put("P", true, yanky_wrappers.blockwise())
 end)
 
 vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")

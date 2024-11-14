f2cdead2402f2234ec03f6be2d086b332bd97b96
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-11-15 01:25:21 +0900

    .

--- nvim/lua/kiyoon/lazy.lua
+++ nvim/lua/kiyoon/lazy.lua
@@ -1359,7 +1359,7 @@ return {
           },
         },
       },
-      -- "folke/neodev.nvim",
+      -- { "saghen/blink.cmp" },
     },
     config = function()
       require "kiyoon.lsp"
@@ -1383,6 +1383,56 @@ return {
       require "kiyoon.cmp"
     end,
   },
+  -- {
+  --   "saghen/blink.cmp",
+  --   lazy = false, -- lazy loading handled internally
+  --   -- optional: provides snippets for the snippet source
+  --   dependencies = {
+  --     "rafamadriz/friendly-snippets",
+  --   },
+  --   -- use a release tag to download pre-built binaries
+  --   version = "v0.*",
+  --
+  --   ---@module 'blink.cmp'
+  --   ---@type blink.cmp.Config
+  --   opts = {
+  --     -- 'default' for mappings similar to built-in completion
+  --     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+  --     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+  --     -- see the "default configuration" section below for full documentation on how to define
+  --     -- your own keymap.
+  --     keymap = { preset = "default" },
+  --
+  --     highlight = {
+  --       -- sets the fallback highlight groups to nvim-cmp's highlight groups
+  --       -- useful for when your theme doesn't support blink.cmp
+  --       -- will be removed in a future release, assuming themes add support
+  --       use_nvim_cmp_as_default = true,
+  --     },
+  --     -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+  --     -- adjusts spacing to ensure icons are aligned
+  --     nerd_font_variant = "mono",
+  --
+  --     -- experimental auto-brackets support
+  --     -- accept = { auto_brackets = { enabled = true } }
+  --
+  --     -- experimental signature help support
+  --     -- trigger = { signature_help = { enabled = true } }
+  --
+  --     windows = {
+  --       autocomplete = {
+  --         -- Controls how the completion items are selected
+  --         -- 'preselect' will automatically select the first item in the completion list
+  --         -- 'manual' will not select any item by default
+  --         -- 'auto_insert' will not select any item by default, and insert the completion items automatically when selecting them
+  --         selection = "auto_insert",
+  --       },
+  --     },
+  --   },
+  --   -- allows extending the enabled_providers array elsewhere in your config
+  --   -- without having to redefining it
+  --   opts_extend = { "sources.completion.enabled_providers" },
+  -- },
   -- {
   --   "kiyoon/lsp-inlayhints.nvim",
   --   event = "LSPAttach",
--- nvim/lua/kiyoon/lsp/handlers.lua
+++ nvim/lua/kiyoon/lsp/handlers.lua
@@ -44,6 +44,10 @@ if status_cmp_ok then
   M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 end
 
+-- blink.cmp
+-- M.capabilities = vim.lsp.protocol.make_client_capabilities()
+-- M.capabilities = require("blink.cmp").get_lsp_capabilities(M.capabilities)
+
 ---@param diagnostic vim.Diagnostic
 ---@return string
 local function format_float(diagnostic)

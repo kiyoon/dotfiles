2aecfc0b064887cb70f0fbe7bebd497c6e6d8304
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-01-03 15:33:05 +0900

    .

---
+++ oh-my-zsh/custom/aliases_mac.zsh
@@ -0,0 +1,9 @@
+# Patch coreutils to use GNU coreutils on Mac.
+# For example, `rm some_dir -rf` will not work on Mac by default.
+
+if [[ $OSTYPE == "darwin"* ]]; then
+	if (($+commands[coreutils])); then
+		alias cp='coreutils cp'
+		alias rm='coreutils rm'
+	fi
+fi

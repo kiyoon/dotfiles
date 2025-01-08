b478327422181d94ced14bf4126872cee0de559d
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-01-08 13:47:33 +0900

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -307,7 +307,7 @@ function groot { & git rev-parse --show-toplevel }
 function glr { & git pull --rebase $args }
 function git_checkout_b { & git checkout -b $args }
 Set-Alias gcb git_checkout_b -Option AllScope
-function git_checkout { & git checkout -b $args }
+function git_checkout { & git checkout $args }
 Set-Alias gco git_checkout -Option AllScope
 function cdg { & cd $(git rev-parse --show-toplevel) }
 # Print the URL of the current repository

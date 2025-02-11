fd04e6d412d9d47acf3a591905f640495ffe135d
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-02-11 13:38:10 +0900

    .

--- oh-my-zsh/custom/00_fake_keys.zsh
+++ oh-my-zsh/custom/00_fake_keys.zsh
@@ -2,18 +2,18 @@
 # This file is used when 00_keys.zsh is not loaded (so nvim chatgpt wouldn't throw an error)
 export OPENAI_API_KEY="sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
-# export LANGUAGE=es_ES.UTF-8
+# export LANGUAGE=es:it:pt_BR:fr:en
 # export LANG=es_ES.UTF-8
 # export LC_ALL=es_ES.UTF-8
 
-# export LANGUAGE=fr_FR.UTF-8
+# export LANGUAGE=fr:it:pt_BR:es:en
 # export LANG=fr_FR.UTF-8
 # export LC_ALL=fr_FR.UTF-8
 
-# export LANGUAGE=pt_BR.UTF-8
+# export LANGUAGE=pt_BR:es:it:fr:en
 # export LANG=pt_BR.UTF-8
 # export LC_ALL=pt_BR.UTF-8
 
-# export LANGUAGE=it_IT.UTF-8
+# export LANGUAGE=it:pt_BR:fr:es:en
 # export LANG=it_IT.UTF-8
 # export LC_ALL=it_IT.UTF-8

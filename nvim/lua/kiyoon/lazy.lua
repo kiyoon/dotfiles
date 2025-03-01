c5bfbbaa489a7c3328042986110731029ebefd2f
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-03-01 20:28:20 +0900

    .

--- nvim/lua/kiyoon/lazy.lua
+++ nvim/lua/kiyoon/lazy.lua
@@ -463,6 +463,61 @@ return {
     end,
   },
   {
+    "y3owk1n/undo-glow.nvim",
+    version = "*", -- remove this if you want to use the `main` branch
+    opts = {
+      animation = {
+        enabled = true,
+        duration = 500,
+        animtion_type = "zoom",
+      },
+      highlights = {
+        undo = {
+          hl_color = { bg = "#693232" }, -- Dark muted red
+        },
+        redo = {
+          hl_color = { bg = "#2F4640" }, -- Dark muted green
+        },
+        yank = {
+          hl_color = { bg = "#7A683A" }, -- Dark muted yellow
+        },
+        paste = {
+          hl_color = { bg = "#325B5B" }, -- Dark muted cyan
+        },
+        search = {
+          hl_color = { bg = "#5C475C" }, -- Dark muted purple
+        },
+        comment = {
+          hl_color = { bg = "#7A5A3D" }, -- Dark muted orange
+        },
+        cursor = {
+          hl_color = { bg = "#793D54" }, -- Dark muted pink
+        },
+      },
+      priority = 2048 * 3,
+    },
+    keys = {
+      {
+        "u",
+        function()
+          require("undo-glow").undo()
+        end,
+        mode = "n",
+        desc = "Undo with highlight",
+        noremap = true,
+      },
+      {
+        "<C-r>",
+        function()
+          require("undo-glow").redo()
+        end,
+        mode = "n",
+        desc = "Redo with highlight",
+        noremap = true,
+      },
+    },
+  },
+  {
     "github/copilot.vim",
     -- event = "InsertEnter",
     -- cmd = { "Copilot" },

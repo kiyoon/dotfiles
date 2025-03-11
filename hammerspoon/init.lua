0e0207ea3b08e7990c7f440428e4136191c55fa6
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-03-11 11:07:21 +0900

    .

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,14 +1,29 @@
 hs.loadSpoon("EmmyLua") -- LSP for hammerspoon
 
--- karabiner-elements maps Rcmd and Ralt to F18
--- Korean-English input source switch
--- when in Wezterm and inside nvim, press ctrl+h (activate hanguel.vim plugin)
+---If there are mutliple active panes, return the last one.
+---Beecause there is an output bug in tmux where it prints another window wrongly and the actual content starts from let's say the 3rd line.
+---The wrong output is not visible in the terminal, but it is visible in the output of `wezterm cli get-text`.
+---@param term_text_ansi string
+---@return string?
+local function get_tmux_current_command(term_text_ansi)
+  -- " <command> ─" with the right color (active pane)
+  local tmux_active_pane_ansi_pattern = [[ (%a+) .%[38:2::98:114:164m.%[49m─]]
+  local current_command_final
+  for current_command in string.gmatch(term_text_ansi, tmux_active_pane_ansi_pattern) do
+    current_command_final = current_command
+  end
+  return current_command_final
+end
 
-local function is_nvim_command_mode(ansi_text)
+---nvim이 command mode인지 확인
+---lualine 왼쪽 "COMMAND" 혹은 오른쪽 "  " 색깔로 구분. tokyonight theme 가정. command mode nvim이 여러개 있지 않다는 가정..
+---@param term_text_ansi string
+---@return boolean
+local function is_nvim_command_mode(term_text_ansi)
   if
-    string.match(ansi_text, [[ .%[38:2::27:29:43m.%[48:2::255:199:119m ]])
+    string.match(term_text_ansi, [[ .%[38:2::27:29:43m.%[48:2::255:199:119m ]])
     or string.match(
-      ansi_text,
+      term_text_ansi,
       [[.%[38:2::27:29:43m.%[48:2::255:199:119m COMMAND .%[38:2::255:199:119m.%[48:2::59:66:97m ]]
     )
   then
@@ -16,6 +31,10 @@ local function is_nvim_command_mode(ansi_text)
   end
   return false
 end
+
+-- karabiner-elements maps Rcmd and Ralt to F18
+-- Korean-English input source switch
+-- when in Wezterm and inside nvim, press ctrl+h (activate hanguel.vim plugin)
 -- 동작 원리
 -- 1. wezterm인지 확인
 -- 2. window title이 vi 인지 확인 -> ctrl+i
@@ -45,7 +64,8 @@ hs.hotkey.bind({}, "f18", function()
       or string.match(window_title, "^nvim$")
     then
       print("program in wezterm is vi")
-      if not is_nvim_command_mode(output) then
+      local output, status, type, rc = hs.execute("/opt/homebrew/bin/wezterm cli get-text --escapes")
+      if status == true and type == "exit" and rc == 0 and output ~= nil and not is_nvim_command_mode(output) then
         if input_source ~= "org.youknowone.inputmethod.Gureum.qwerty" then
           hs.keycodes.currentSourceID("org.youknowone.inputmethod.Gureum.qwerty")
         end
@@ -63,6 +83,7 @@ hs.hotkey.bind({}, "f18", function()
       -- Run `wezterm cli get-text` to get the text of the pane
       local output, status, type, rc = hs.execute("/opt/homebrew/bin/wezterm cli get-text --escapes")
       -- print(output)
+      print(get_tmux_current_command(output))
 
       -- match the output to detect if the pane border title has nvim
       -- more specifically, nvim ─ with the right color (active pane)
@@ -73,20 +94,16 @@ hs.hotkey.bind({}, "f18", function()
         and type == "exit"
         and rc == 0
         and output ~= nil
-        -- and not string.match(output, [[nvim .%[38:2::98:114:164m.%[49m%[38:2::68:71:90m]])
+        and get_tmux_current_command(output) == "nvim"
       then
-        -- drop the first line of the output because in tmux usually it prints another window wrongly and the actual content starts from let's say the 3rd line.
-        output = string.match(output, "\n(.*)")
-        if string.match(output, [[nvim% .%[38:2::98:114:164m.%[49m───]]) then
-          print("nvim in tmux")
-          if not is_nvim_command_mode(output) then
-            print("not in command mode")
-            if input_source ~= "org.youknowone.inputmethod.Gureum.qwerty" then
-              hs.keycodes.currentSourceID("org.youknowone.inputmethod.Gureum.qwerty")
-            end
-            hs.eventtap.keyStroke({ "ctrl" }, "i")
-            return
+        print("nvim in tmux")
+        if not is_nvim_command_mode(output) then
+          print("not in command mode")
+          if input_source ~= "org.youknowone.inputmethod.Gureum.qwerty" then
+            hs.keycodes.currentSourceID("org.youknowone.inputmethod.Gureum.qwerty")
           end
+          hs.eventtap.keyStroke({ "ctrl" }, "i")
+          return
         end
       end
     end

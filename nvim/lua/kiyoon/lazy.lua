b52772967733d9fa6e3cf21b8658d62537d272a5
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-04-16 14:30:15 +0900

    .

--- nvim/lua/kiyoon/lazy.lua
+++ nvim/lua/kiyoon/lazy.lua
@@ -216,11 +216,11 @@ return {
     "kiyoon/jupynium.nvim",
     build = "bash scripts/build_with_uv.sh ~/.virtualenvs/jupynium",
     -- build = "uv pip install . --python=$HOME/.virtualenvs/jupynium/bin/python",
-    -- ft = { "python", "markdown" },
-    cmd = {
-      "JupyniumStartAndAttachToServer",
-      "JupyniumStartAndAttachToServerInTerminal",
-    },
+    ft = { "python", "markdown" },
+    -- cmd = {
+    --   "JupyniumStartAndAttachToServer",
+    --   "JupyniumStartAndAttachToServerInTerminal",
+    -- },
     config = function()
       local python_host
       if jupynium_dev then
@@ -1613,153 +1613,153 @@ return {
   },
   -- optional for lazydev.nvim: `vim.uv` typings. Plugin will never be loaded
   { "Bilal2453/luvit-meta", lazy = true },
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   -- event = "InsertEnter",
-  --   dependencies = {
-  --     "hrsh7th/cmp-nvim-lsp",
-  --     "hrsh7th/cmp-buffer",
-  --     "hrsh7th/cmp-path",
-  --     "hrsh7th/cmp-calc",
-  --     "hrsh7th/cmp-emoji",
-  --     "saadparwaiz1/cmp_luasnip",
-  --     "onsails/lspkind.nvim", -- display icons
-  --   },
-  --   config = function()
-  --     require("kiyoon.cmp")
-  --   end,
-  -- },
   {
-    "saghen/blink.cmp",
-    -- optional: provides snippets for the snippet source
+    "hrsh7th/nvim-cmp",
+    -- event = "InsertEnter",
     dependencies = {
-      "rafamadriz/friendly-snippets",
-      "MahanRahmati/blink-nerdfont.nvim",
-      "moyiz/blink-emoji.nvim",
-      "L3MON4D3/LuaSnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-calc",
+      "hrsh7th/cmp-emoji",
+      "saadparwaiz1/cmp_luasnip",
+      "onsails/lspkind.nvim", -- display icons
     },
-
-    -- use a release tag to download pre-built binaries
-    version = "1.*",
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = {
-        -- set to 'none' to disable the 'default' preset
-        preset = "default",
-        -- disable a keymap from the preset
-        ["<Up>"] = {},
-        ["<Down>"] = {},
-      },
-
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = "mono",
-      },
-
-      completion = {
-        menu = { border = "rounded" },
-        documentation = {
-          auto_show = true,
-          window = { border = "rounded" },
-        },
-      },
-
-      signature = {
-        enabled = false,
-        window = {
-          border = "rounded",
-          show_documentation = true,
-        },
-      },
-
-      snippets = { preset = "luasnip" },
-
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = {
-          "jupynium",
-          "lazydev",
-          "lsp",
-          "path",
-          "snippets",
-          "buffer",
-          "nerdfont",
-          "emoji",
-        },
-        providers = {
-          jupynium = {
-            name = "Jupynium",
-            module = "jupynium.blink_cmp",
-            -- Consider higher priority than LSP
-            score_offset = 100,
-          },
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-            enabled = function()
-              return vim.bo.filetype == "lua"
-            end,
-          },
-          lsp = {
-            score_offset = 90,
-          },
-          snippets = {
-            score_offset = 50,
-          },
-          nerdfont = {
-            module = "blink-nerdfont",
-            name = "Nerd Fonts",
-            score_offset = 15, -- Tune by preference
-            opts = { insert = true }, -- Insert nerdfont icon (default) or complete its name
-          },
-          emoji = {
-            module = "blink-emoji",
-            name = "Emoji",
-            score_offset = 16, -- Tune by preference
-            opts = { insert = true }, -- Insert emoji (default) or complete its name
-            -- should_show_items = function()
-            --   return vim.tbl_contains(
-            --     -- Enable emoji completion only for git commits and markdown.
-            --     -- By default, enabled for all file-types.
-            --     { "gitcommit", "markdown" },
-            --     vim.o.filetype
-            --   )
-            -- end,
-          },
-        },
-      },
-
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "prefer_rust_with_warning" },
-    },
-    opts_extend = { "sources.default" },
+    config = function()
+      require("kiyoon.cmp")
+    end,
   },
+  -- {
+  --   "saghen/blink.cmp",
+  --   -- optional: provides snippets for the snippet source
+  --   dependencies = {
+  --     "rafamadriz/friendly-snippets",
+  --     "MahanRahmati/blink-nerdfont.nvim",
+  --     "moyiz/blink-emoji.nvim",
+  --     "L3MON4D3/LuaSnip",
+  --   },
+  --
+  --   -- use a release tag to download pre-built binaries
+  --   version = "1.*",
+  --   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  --   -- build = 'cargo build --release',
+  --   -- If you use nix, you can build from source using latest nightly rust with:
+  --   -- build = 'nix run .#build-plugin',
+  --
+  --   ---@module 'blink.cmp'
+  --   ---@type blink.cmp.Config
+  --   opts = {
+  --     -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+  --     -- 'super-tab' for mappings similar to vscode (tab to accept)
+  --     -- 'enter' for enter to accept
+  --     -- 'none' for no mappings
+  --     --
+  --     -- All presets have the following mappings:
+  --     -- C-space: Open menu or open docs if already open
+  --     -- C-n/C-p or Up/Down: Select next/previous item
+  --     -- C-e: Hide menu
+  --     -- C-k: Toggle signature help (if signature.enabled = true)
+  --     --
+  --     -- See :h blink-cmp-config-keymap for defining your own keymap
+  --     keymap = {
+  --       -- set to 'none' to disable the 'default' preset
+  --       preset = "default",
+  --       -- disable a keymap from the preset
+  --       ["<Up>"] = {},
+  --       ["<Down>"] = {},
+  --     },
+  --
+  --     appearance = {
+  --       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+  --       -- Adjusts spacing to ensure icons are aligned
+  --       nerd_font_variant = "mono",
+  --     },
+  --
+  --     completion = {
+  --       menu = { border = "rounded" },
+  --       documentation = {
+  --         auto_show = true,
+  --         window = { border = "rounded" },
+  --       },
+  --     },
+  --
+  --     signature = {
+  --       enabled = false,
+  --       window = {
+  --         border = "rounded",
+  --         show_documentation = true,
+  --       },
+  --     },
+  --
+  --     snippets = { preset = "luasnip" },
+  --
+  --     -- Default list of enabled providers defined so that you can extend it
+  --     -- elsewhere in your config, without redefining it, due to `opts_extend`
+  --     sources = {
+  --       default = {
+  --         "jupynium",
+  --         "lazydev",
+  --         "lsp",
+  --         "path",
+  --         "snippets",
+  --         "buffer",
+  --         "nerdfont",
+  --         "emoji",
+  --       },
+  --       providers = {
+  --         jupynium = {
+  --           name = "Jupynium",
+  --           module = "jupynium.blink_cmp",
+  --           -- Consider higher priority than LSP
+  --           score_offset = 100,
+  --         },
+  --         lazydev = {
+  --           name = "LazyDev",
+  --           module = "lazydev.integrations.blink",
+  --           -- make lazydev completions top priority (see `:h blink.cmp`)
+  --           score_offset = 100,
+  --           enabled = function()
+  --             return vim.bo.filetype == "lua"
+  --           end,
+  --         },
+  --         lsp = {
+  --           score_offset = 90,
+  --         },
+  --         snippets = {
+  --           score_offset = 50,
+  --         },
+  --         nerdfont = {
+  --           module = "blink-nerdfont",
+  --           name = "Nerd Fonts",
+  --           score_offset = 15, -- Tune by preference
+  --           opts = { insert = true }, -- Insert nerdfont icon (default) or complete its name
+  --         },
+  --         emoji = {
+  --           module = "blink-emoji",
+  --           name = "Emoji",
+  --           score_offset = 16, -- Tune by preference
+  --           opts = { insert = true }, -- Insert emoji (default) or complete its name
+  --           -- should_show_items = function()
+  --           --   return vim.tbl_contains(
+  --           --     -- Enable emoji completion only for git commits and markdown.
+  --           --     -- By default, enabled for all file-types.
+  --           --     { "gitcommit", "markdown" },
+  --           --     vim.o.filetype
+  --           --   )
+  --           -- end,
+  --         },
+  --       },
+  --     },
+  --
+  --     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+  --     -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+  --     -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+  --     --
+  --     -- See the fuzzy documentation for more information
+  --     fuzzy = { implementation = "prefer_rust_with_warning" },
+  --   },
+  --   opts_extend = { "sources.default" },
+  -- },
   -- {
   --   "kiyoon/lsp-inlayhints.nvim",
   --   event = "LSPAttach",

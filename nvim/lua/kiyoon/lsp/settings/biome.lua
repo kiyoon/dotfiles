e0f4de8e15356213829df83f76922ce4bc915ba3
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-04-16 16:57:40 +0900

    .

--- nvim/lua/kiyoon/lsp/settings/biome.lua
+++ nvim/lua/kiyoon/lsp/settings/biome.lua
@@ -1,17 +1,23 @@
+local util = require("lspconfig.util")
+
 return {
   -- https://github.com/neovim/nvim-lspconfig/pull/2984
   -- by default, biome is active only when biome.json is configured.
   -- this changes the behavior to be active with single files.
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
-    return vim.fs.dirname(vim.fs.find({
+    local root_files = {
       "biome.json",
       "biome.jsonc",
-      "package.json",
+      -- "package.json",
       "node_modules",
       ".git",
-    }, { path = fname, upward = true })[1])
+    }
+    root_files = util.insert_package_json(root_files, "biome", fname)
+    local root_dir = vim.fs.dirname(vim.fs.find(root_files, { path = fname, upward = true })[1])
+    on_dir(root_dir)
   end,
   single_file_support = true,
+  -- workspace_required = false,
   -- settings = {},
 }

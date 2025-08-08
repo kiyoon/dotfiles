0eb70cc0864f6c63aa0e06b9e17c489d9a7431cf
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-08-08 11:00:51 +0900

    .

--- nvim/lua/kiyoon/lsp/settings/ts_ls.lua
+++ nvim/lua/kiyoon/lsp/settings/ts_ls.lua
@@ -1,5 +1,16 @@
 -- https://github.com/lvimuser/lsp-inlayhints.nvim
+
+-- root finds bun.lock first if exists, otherwise it will use tsconfig.json
+-- This is useful for monorepos where many projects share references.
+-- If you don't set the root correctly, it will not find the references correctly.
+
+local root = vim.fs.root(0, "bun.lock")
+if root == nil then
+  root = vim.fs.root(0, { "tsconfig.json", "jsconfig.json", "package.json", ".git" })
+end
+
 return {
+  root_dir = root,
   settings = {
     typescript = {
       inlayHints = {

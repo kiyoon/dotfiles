f774a3d1c588956d7d711e36b25a2cfb1c2568cd
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-08-11 16:49:40 +0900

    .

--- nvim/treemux_init.lua
+++ nvim/treemux_init.lua
@@ -278,6 +278,34 @@ require("lazy").setup({
     end,
   },
   {
+    "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>un",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Delete all Notifications",
+      },
+    },
+    opts = {
+      stages = "fade_in_slide_out",
+      -- stages = "slide",
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    },
+    config = function(_, opts)
+      require("notify").setup(opts)
+      vim.notify = require("notify")
+    end,
+  },
+  {
     "aserowy/tmux.nvim",
     keys = {
       "<C-h>",

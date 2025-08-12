987517a144ad0bd84865af271715c0f2d7c08d07
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-08-12 10:43:26 +0900

    .

--- nvim/luasnippets/sh.lua
+++ nvim/luasnippets/sh.lua
@@ -1,6 +1,7 @@
-local ls = require "luasnip"
+local url_utils = require("kiyoon.utils.url")
+local ls = require("luasnip")
 local fmt = require("luasnip.extras.fmt").fmt
-local extras = require "luasnip.extras"
+local extras = require("luasnip.extras")
 local l = extras.lambda
 local i = ls.insert_node
 local c = ls.choice_node
@@ -10,30 +11,22 @@ local s = ls.snippet
 local sn = ls.snippet_node
 local t = ls.text_node
 
-local function read_from_url(url)
-  -- curl without progress bar
-  local content = vim.fn.system("curl -s " .. url)
-  -- split by newline
-  local split_content = vim.fn.split(content, "\n")
-  return split_content
-end
-
 -- require snippets
 return {
   -- Get the directory of the script
   s("sdir", {
-    t {
+    t({
       [[SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )]],
       [[]],
-    },
+    }),
   }),
   s("waitp", {
-    t {
+    t({
       [[# wait for shell's child process to exit]],
       [[shell_pid=]],
-    },
+    }),
     i(1, "pid"),
-    t {
+    t({
       "",
       [[child_pid=$(ps -ef | awk -v shell_pid=$shell_pid '$3==shell_pid {print $2}')]],
       [[echo "child_pid: $child_pid"]],
@@ -41,17 +34,17 @@ return {
       "\tsleep 1",
       [[done]],
       "",
-    },
+    }),
     i(0),
   }),
   s("tmuxrun", {
-    t {
+    t({
       [[# Run multiple commands in a tmux session]],
       [[script_dir=$(dirname "$(realpath -s "$0")")]],
       [[sess=]],
-    },
+    }),
     i(1, "session_name"),
-    t {
+    t({
       "",
       [[tmux new -d -s "$sess" -c "$script_dir"   # Use default directory as this script directory]],
       "",
@@ -60,35 +53,37 @@ return {
       "\t" .. [[# Window 0 or 1 may already exist so it will print error. Ignore that.]],
       "\t" .. [[tmux new-window -t "$sess:$window"]],
       "\t" .. [[command="CUDA_VISIBLE_DEVICES=$window ]],
-    },
+    }),
     i(2, [[python train.py --arg $((window+1))]]),
-    t {
+    t({
       [["]],
       "\t" .. [[tmux send-keys -t "$sess:$window" "$command" Enter]],
       [[done]],
       [[]],
-    },
+    }),
     i(0),
   }),
   s("tmuxp", {
     func_node(function()
-      local content =
-        read_from_url "https://gist.githubusercontent.com/kiyoon/8d9ab895d2f478cde2c7fec214d55dbb/raw/run_tmux_parallel.sh"
+      local content = url_utils.read_from_url(
+        "https://gist.githubusercontent.com/kiyoon/8d9ab895d2f478cde2c7fec214d55dbb/raw/run_tmux_parallel.sh"
+      )
       return content
     end, {}),
   }),
   s("tmuxps", {
     func_node(function()
-      local content =
-        read_from_url "https://gist.githubusercontent.com/kiyoon/8d9ab895d2f478cde2c7fec214d55dbb/raw/tmux_parallel_status.sh"
+      local content = url_utils.read_from_url(
+        "https://gist.githubusercontent.com/kiyoon/8d9ab895d2f478cde2c7fec214d55dbb/raw/tmux_parallel_status.sh"
+      )
       return content
     end, {}),
   }),
   s("orx", {
     -- For safe cd
     -- https://www.shellcheck.net/wiki/SC2164
-    t {
+    t({
       [[|| { echo "Failure"; exit 1; }]],
-    },
+    }),
   }),
 }

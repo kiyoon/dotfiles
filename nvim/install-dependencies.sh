2057d091773dc338cc1b2e90e039ab762e41baca
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-08-14 16:09:33 +0900

    .

--- nvim/install-dependencies.sh
+++ nvim/install-dependencies.sh
@@ -117,23 +117,19 @@ fi
 # molten.nvim
 # We need to extract the AppImage because the luarocks magick (bundled in this dotfiles)
 # requires the shared libraries.
-if ! command -v magick &> /dev/null; then
-	echo "ImageMagick could not be found. Installing in $LOCALBIN"
-	mkdir -p ~/.local
-	curl -s https://api.github.com/repos/ImageMagick/ImageMagick/releases/latest \
-		| grep "browser_download_url.*ImageMagick-.*-gcc-x86_64.AppImage" \
-		| cut -d : -f 2,3 \
-		| tr -d \" \
-		| wget -qi - -O magick.appimage
-	chmod +x magick.appimage
-	./magick.appimage --appimage-extract
-	# NOTE: installing libglib will break the system's package manager.
-	# Many apps depend on it and it won't work.
-	# We need to remove libglib from the extracted AppImage.
-	rm squashfs-root/usr/lib/libglib-2.0.so.0
-	rsync -a squashfs-root/usr/ ~/.local/
-	rm magick.appimage
-	rm -rf squashfs-root
-else
-	echo "ImageMagick found at $(which magick). Skipping installation."
-fi
+INSTALL_DIR=$(nvim --clean --headless +'lua io.write(vim.fn.stdpath("data"))' +qa)/magick  # ~/.local/share/nvim/magick
+mkdir -p "$INSTALL_DIR"
+curl -s https://api.github.com/repos/ImageMagick/ImageMagick/releases/latest \
+    | grep "browser_download_url.*ImageMagick-.*-gcc-x86_64.AppImage" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi - -O magick.appimage
+chmod +x magick.appimage
+./magick.appimage --appimage-extract
+# NOTE: installing libglib will break the system's package manager.
+# Many apps depend on it and it won't work.
+# We need to remove libglib from the extracted AppImage.
+rm squashfs-root/usr/lib/libglib-2.0.so.0
+rsync -a squashfs-root/usr/ "$INSTALL_DIR"/
+rm magick.appimage
+rm -rf squashfs-root

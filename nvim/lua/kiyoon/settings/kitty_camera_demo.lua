a4dd1148421b57e19ecc408df419619ad7822956
Kiyoon Kim <kiyoon@users.noreply.github.com>
2026-09-01 22:21:00 +0900

    Add opt-in Kitty camera demo for Nvim

---
+++ nvim/lua/kiyoon/settings/kitty_camera_demo.lua
@@ -0,0 +1,23 @@
+local demo_env = "KITTY_CAMERA_DEMO"
+local allowed_demo = "nvim-tour"
+
+local function emit_demo(name)
+  local osc = ("\27]1337;SetUserVar=camera_demo=%s\7"):format(vim.base64.encode(name))
+  if vim.env.TMUX and vim.env.TMUX ~= "" then
+    osc = "\27Ptmux;\27" .. osc .. "\27\\"
+  end
+  vim.api.nvim_ui_send(osc)
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  once = true,
+  callback = function()
+    if vim.env[demo_env] ~= allowed_demo then
+      return
+    end
+    vim.env[demo_env] = nil
+    if #vim.api.nvim_list_uis() > 0 then
+      emit_demo(allowed_demo)
+    end
+  end,
+})
